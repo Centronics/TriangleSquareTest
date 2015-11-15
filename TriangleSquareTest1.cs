ad09530231578e1f8c748effdb7d3bf4cdc255ab
Илья Красильников <kras-05@mail.ru>
2015-11-15 17:27:13 +0300

    Добавление отлаженного тестового проекта.

---
+++ TriangleSquareTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TriangleSquareTest
+{
+    [TestClass]
+    public class TriangleSquareTest1
+    {
+        [TestMethod]
+        public void TestMethodTriangleSquare()
+        {
+            double expected = 693.0, actual = Triangle.Triangle.TriangleSquare(12.0, 115.5);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethodTriangleSquareExpt()
+        {
+            Triangle.Triangle.TriangleSquare(-12.0, 115.5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethodTriangleSquareExpt1()
+        {
+            Triangle.Triangle.TriangleSquare(0.0, -115.5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethodTriangleSquareExpt2()
+        {
+            Triangle.Triangle.TriangleSquare(12.0, -115.5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethodTriangleSquareExpt3()
+        {
+            Triangle.Triangle.TriangleSquare(-115.5, 0.0);
+        }
+    }
+}
